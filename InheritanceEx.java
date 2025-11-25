7d639ba3ed4d2a4d19753bd790aa84b69e583a09
saipriyakolipaka <kolipakasaipriya6789@gmail.com>
2025-11-25 15:15:44 +0530

    Inheritance

---
+++ InheritanceEx.java
@@ -0,0 +1,16 @@
+class Animal{
+void sound(){
+System.out.println("Animal makes sounds");
+}
+class Dog extends Animal{
+void  barks(){
+System.out.println("Dog is an animal");
+}
+}
+public class InheritanceEx{
+public static void main(String[] args){
+Dog d=new Dog();
+d.sound();
+d.barks();
+}
+}
